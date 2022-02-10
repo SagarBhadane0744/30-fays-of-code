ad209a9765106d1de391abae6775cee0b5794ce0
Satish Mane <manes1777@gmail.com>
2022-02-10 11:50:52 +0530

    Update storyedit.cpp

--- storyedit.cpp
+++ storyedit.cpp
@@ -5,11 +5,11 @@ int main(){
     string charname;
     int charage;
     cout<< " Enter the character name: ";
-    cin>>charname;
+    cin>> charname;
     cout<< "Enter characters age: ";
-    cin>>charage;
+    cin>> charage;
     cout<< "There once was a man named "<< charname << endl;
     cout<< "He was " << charage <<" Years old" << endl;
     cout<< "He liked the name " << charname << endl;
     cout<< "But he did not like being " << charage << endl;
-}
+} 
