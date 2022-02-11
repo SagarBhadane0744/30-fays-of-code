32e8d86cffac7afb6049c70da1abcdff6362f0bb
Satish Mane <manes1777@gmail.com>
2022-02-11 10:49:18 +0530

    know the length of the string

---
+++ calenth.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    string phrase;
+    phrase = "I am very happy today, this is the great day of my life";
+    cout << phrase.length(); 
+}
