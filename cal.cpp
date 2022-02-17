d63059ff08c81bf10b4c766e7eb967836aca64e7
Satish Mane <manes1777@gmail.com>
2022-02-17 15:26:31 +0530

    Create cal.cpp

---
+++ cal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2, sum;
+
+    cout << " Enter first number: ";
+    cin >> num1;
+
+    cout << " Enter an Another number: ";
+    cin >> num2;
+    sum = num1+num2;
+    cout <<sum;
+
+}
