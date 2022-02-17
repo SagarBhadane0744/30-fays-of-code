7ea66a23684f36e66cc189a91d18dc3f40700029
Satish Mane <manes1777@gmail.com>
2022-02-17 14:50:06 +0530

    Create math.cpp

---
+++ math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    cout << pow(2, 10) << endl; // this is the power function.
+
+    cout << sqrt(25) << endl; // this can be give us square root of given number.
+
+    cout << round(8.6); // this can be give us a rounded value (an integer value).
+
+}
