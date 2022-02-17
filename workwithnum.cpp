e0b967396a787cbf811f80a7923fc929539638e8
Satish Mane <manes1777@gmail.com>
2022-02-17 14:38:01 +0530

    Update workwithnum.cpp

--- workwithnum.cpp
+++ workwithnum.cpp
@@ -9,9 +9,19 @@ int main(){
     //this is also a method to do the calculation and all stuff
 
         int wnum = 5;
+        int num = 14;
+        int num1 = 125;
         double dnum = 4.2;
 
         wnum++; //this can be add 1 in already stored variable.
 
-        cout << wnum;
+        num--; //this can be remove 1 in already stored variable.
+
+        num1 += 30; //this can be add "30" in already stored variable.
+
+        cout << wnum << endl;
+
+        cout << num<< endl;
+
+        cout << num1;
 }
