f892af60d8ed79bb4569d6bfe770af42910f09d3
Satish Mane <manes1777@gmail.com>
2022-02-17 14:58:29 +0530

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -9,6 +9,10 @@ int main(){
 
     cout << sqrt(25) << endl; // this can be give us square root of given number.
 
-    cout << round(8.6); // this can be give us a rounded value (an integer value).
+    cout << round(8.6) << endl; // this can be give us a rounded value (an integer value).
+
+    cout << fmax(10, 5) << endl;// find maximum in two numbers.
+
+    cout << fmin(126, 912) << endl;// find minimum in two numbers.
 
 }
