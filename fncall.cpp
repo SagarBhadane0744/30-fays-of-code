769ace492f432dcff55b5be1a034b6e54958c766
Satish Mane <manes1777@gmail.com>
2022-02-22 18:28:16 +0530

    Update fncall.cpp

--- fncall.cpp
+++ fncall.cpp
@@ -4,5 +4,14 @@ using namespace std;
 int DemoConsoleOutput();
 //this is the simple declaration of the function
 int main(){
+    DemoConsoleOutput();
+    return 0;
+}
+int DemoConsoleoutput(){
+    cout << "This is a Simple string literal" << endl;
+    cout << "Writing Number : " << 5 << endl;
+    cout << "Perform Simple Division 10 / 5 = " << 10 / 5 << endl;
+    cout << " pi is 32 / 7 = " << 22.0 / 7 << endl;
+
     return 0;
 }
