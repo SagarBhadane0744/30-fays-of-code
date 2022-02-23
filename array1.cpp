c2c97b54e9176a84639fb27417afa98fc3a2795d
Satish Mane <manes1777@gmail.com>
2022-02-23 16:26:29 +0530

    Update array1.cpp

--- array1.cpp
+++ array1.cpp
@@ -5,5 +5,10 @@ int main(){
     
     int LuckyNames[] = {4, 8, 23, 45, 23, 42, 15};
     
-    cout << LuckyNames[3];
+    cout << LuckyNames[3] << endl;
+    
+    LuckyNames[0] = 15; // asign a different values.
+
+    cout << LuckyNames[0];
+
 }
