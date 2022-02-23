b62dba8c5412391f47f63b4e9b97266e46a9deb5
Satish Mane <manes1777@gmail.com>
2022-02-23 16:33:31 +0530

    Update array1.cpp

--- array1.cpp
+++ array1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     
-    int LuckyNames[] = {4, 8, 23, 45, 23, 42, 15};
+    int LuckyNames[20] = {4, 8, 23, 45, 23, 42, 15};
     
     cout << LuckyNames[3] << endl;
     
