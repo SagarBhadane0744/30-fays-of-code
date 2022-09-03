0bf94d8ad6184c3d7e36f895b7e2fc0218edd077
Satish Mane <manes1777@gmail.com>
2022-09-03 14:32:53 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+/*int main(){
+    int a = 3;
+    int b = a+1;
+    if((a=3)==b){
+
+        cout<<a;
+    }
+    else{
+        cout<<a+1;
+    }
+}*/
+
+
+int main(){
+    int a = 24;
+    if(a>20){
+        cout<<"Love ";
+    }
+    else if(a == 24){
+        cout<<"Lovely ";
+    }
+    else{
+        cout<<"Babbar";
+    }
+    cout<<a;
+}
