a07c005acadd5a625eac7c8a41e60c7fbe62424d
Satish Mane <manes1777@gmail.com>
2022-09-05 07:23:28 +0530

    Update whileloop.cpp

--- whileloop.cpp
+++ whileloop.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+/*int main(){
     int n;   
     int i=1;
-    
+
     cout<<"Enter the value: ";
     cin>>n;
  
@@ -13,4 +13,23 @@ int main(){
     cout<<i<<" ";
     i = i+1;
     }
+}*/
+
+
+int main(){
+    int n;
+
+    cout<<"Enter the Number: ";
+    cin>>n;
+
+    int i = 1;
+    int sum = 0;
+
+    while(i<=n){
+
+        sum = sum + i;
+        i = i + 1;
+
+    }
+    cout<<"The value of sum is "<<sum<<endl;
 }
