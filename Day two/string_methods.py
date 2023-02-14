55b3ef206d4fc460cb62f1c24a0d55181d3391b6
Satish Mane <manes1777@gmail.com>
2023-02-14 21:14:46 +0530

    Update string_methods.py

--- Day two/string_methods.py
+++ Day two/string_methods.py
@@ -2,5 +2,4 @@ name = "brother this is just a joke you get that"
 
 print(len(name)) #this can simply count the len
 
-print(name.find("o")) #this can tell us that how many time this letter is present in the statement
-
+print(name.find("i")) #this can tell us that whare the charecter is present 
