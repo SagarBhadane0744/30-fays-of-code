d307871bedf56339d6fe8f70cd7e1d62464a1ad1
Satish Mane <manes1777@gmail.com>
2023-04-11 04:09:06 +0530

    Delete variables.py

--- Day two/variables.py
+++
@@ -1,6 +0,0 @@
-# its a container of a values behave as the value of that it contain
-#name = "Satish Mane"
-first_name = "Satish"
-last_name = "Mane"
-full_name = first_name + " " + last_name
-print("Hello " +full_name)
