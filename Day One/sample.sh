c12543ed4a0d36298d6d265ffe04c9765720738c
Satish Mane <manes1777@gmail.com>
2023-04-11 04:10:28 +0530

    delete files

--- Day One/sample.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-ls
-ls -s
-mkdir sam
-cd sam
-touch sam1{1..100}.txt
-cd 
-ls -l sam/*.txt
--- Day One/void.cpp
+++
@@ -1,9 +0,0 @@
-// CPP Program to demonstrate main() with
-// return type
-#include <iostream>
-using namespace std;
-// Driver Code
-int main()
-{
-    cout<<"This program returns the integer value 0\n";
-}
